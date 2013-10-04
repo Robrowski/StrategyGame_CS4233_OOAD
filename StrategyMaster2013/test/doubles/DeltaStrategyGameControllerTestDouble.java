ac596f64a754fec0c2aa658bc078061540a2f80c
Robert Dabrowski <rpdabrowski@wpi.edu>
2013-10-04 11:26:49 -0400

    Add DeltaStrategyGameControllerTestDouble
    This test double speeds up the process of testing by providing a new
    method "setFieldConfiguration" that can take either a new board manager
    or a collection of pieces. This allows a tester to make the test double
    with a valid configuration then immediately set the configuration to be
    something more convenient for testing purposes. An example would be to
    set the field to have both players only have a marshal and a flag to
    quickly test end game scenarios.

---
+++ StrategyMaster2013/test/doubles/DeltaStrategyGameControllerTestDouble.java
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ * This files was developed for CS4233: Object-Oriented Analysis & Design.
+ * The course was taken at Worcester Polytechnic Institute.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package doubles;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+import strategy.common.StrategyException;
+import strategy.game.common.PieceLocationDescriptor;
+import strategy.game.common.board.IBoardManager;
+import strategy.game.common.board.MapBoardManager;
+import strategy.game.version.VersionRules;
+import strategy.game.version.delta.DeltaStrategyGameController;
+
+/** Same exact functionality as a DeltaStrategyGameController + the
+ *  internal configuration can be set after initialization
+ * 
+ * 
+ * @author Dabrowski
+ * @version $Revision: 6.9 $
+ */
+public class DeltaStrategyGameControllerTestDouble extends
+		DeltaStrategyGameController {
+
+	/** Constructor for DeltaStrategyGameControllerTestDouble. Takes two configurations, checks their 
+	 *  validity (throwing an exception if either is invalid) and stores them internally,
+	 *  as well as setting up a couple other basic variables involved with a game.
+	 * 
+	 * @param redConfiguration Configuration for the red side
+	 * @param blueConfiguration  Configuration for the blue side
+	 * @param DeltaRules a validator for checking the player configurations
+	 * @throws StrategyException Thrown if an invalid configuration is detected */
+	public DeltaStrategyGameControllerTestDouble(
+			Collection<PieceLocationDescriptor> redConfiguration,
+			Collection<PieceLocationDescriptor> blueConfiguration,
+			VersionRules DeltaRules) throws StrategyException {
+		super(redConfiguration, blueConfiguration, DeltaRules);
+	}
+	
+	/** Sets the current field configuration to the given config in
+	 *  the form of a board manager
+	 * 
+	 * @param toSetTo the board manager to set to 
+	 */
+	public void setFieldConfiguration(IBoardManager toSetTo){
+		fieldConfiguration = toSetTo;
+	}
+	
+	/** Sets the current field configuration to the given config in
+	 *  the form of a board manager
+	 * 
+	 * @param toSetTo collection of pieces to put in the board manager
+	 */
+	public void setFieldConfiguration(Collection<PieceLocationDescriptor> toSetTo){
+		// Make a new manager
+		IBoardManager newManager = new MapBoardManager(new HashMap<String,PieceLocationDescriptor>());
+		// Add the collection to the manager
+		newManager.addToConfiguration(toSetTo);
+		// Sets the field configuration		
+		setFieldConfiguration(newManager);
+	}
+	
+	
+
+}

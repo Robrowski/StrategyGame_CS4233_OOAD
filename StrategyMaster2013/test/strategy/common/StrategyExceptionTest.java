63280e0d94d61a1b593bac227c56af11137c68b3
Robert Dabrowski <rpdabrowski@wpi.edu>
2013-10-04 20:02:51 -0400

    test custom Strategy Exceptions
    test unused methods in strategy exceptions

---
+++ StrategyMaster2013/test/strategy/common/StrategyExceptionTest.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * This files was developed for CS4233: Object-Oriented Analysis & Design.
+ * The course was taken at Worcester Polytechnic Institute.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package strategy.common;
+
+import static org.junit.Assert.assertSame;
+
+import org.junit.Test;
+
+/** Simple tests make sure the custom exceptions work
+ * 
+ * 
+ * @author Dabrowski
+ *
+ */
+public class StrategyExceptionTest {
+
+
+	@Test
+	public void strategyRuntimeException_holdsException(){
+		StrategyRuntimeException sre = new StrategyRuntimeException("A test", new StrategyException("test internal exception"));
+
+		try {
+			throw sre;
+		} catch(StrategyRuntimeException thrownSRE){
+			assertSame(thrownSRE.getCause().getMessage(), "test internal exception");
+		}
+	}
+
+	@Test
+	public void strategyException_holdsException() throws StrategyException{
+		StrategyException se = new StrategyException("A test", new StrategyException("test internal exception"));
+
+		try {
+			throw se;
+		} catch(StrategyException thrownSE){
+			assertSame(thrownSE.getCause().getMessage(), "test internal exception");
+		}
+	}
+}

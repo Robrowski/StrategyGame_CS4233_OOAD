04d6a4b579164ff70484d36cff65e2ad6b877c87
Robert Dabrowski <rpdabrowski@wpi.edu>
2014-05-15 20:58:27 -0400

    More test suite for counting dead pieces

--- StrategyMaster2013/test/game/go/GoDeadPieceCountingTest_Chinese.java
+++ StrategyMaster2013/test/game/go/GoDeadPieceCountingTest_Chinese.java
@@ -9,14 +9,46 @@
  *******************************************************************************/
 package game.go;
 
+import java.util.Collection;
+
+import org.junit.Test;
+
+import game.common.PieceLocationDescriptor;
+import common.PlayerColor;
+import common.StrategyException;
+import static org.junit.Assert.assertTrue;
+
+
+
 /** Tests to specifically test how "dead pieces" are recognized
  * 
  * @author Dabrowski
  *
  */
-public class GoDeadPieceCountingTest extends GoTestSuite {
+public class GoDeadPieceCountingTest_Chinese extends GoTestSuite {
 
+	/** Run a dead piece test. Assume that all black pieces are dead and will be removed.
+	 * 
+	 * @param blk black configuration
+	 * @param wht white configuration
+	 * @throws StrategyException
+	 */
+	void runDeadPieceTest(int[][] blk, int[][] wht) throws StrategyException{
+		addBlackAndWhitePieces(blk,wht);
+		
+		Collection<PieceLocationDescriptor> expectedDead = generateConfiguration(PlayerColor.BLACK, blk);
+		Collection<PieceLocationDescriptor> deadFound = ((GoScoreKeeperTestDouble)  gameDoubleScore).findDeadPieces();
+		
+		assertTrue(expectedDead.containsAll(deadFound) && deadFound.containsAll(expectedDead));
+	}
 	
+	
+	
+	@Test
+	public void noCapturesOnEmptyBoard() throws StrategyException {
+		int[][] nullList = {   };
+		runDeadPieceTest(nullList, nullList);
+	}
 	
 	
 	
